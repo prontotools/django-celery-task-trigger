53c759cd3582cd5a403a1a99a81b70f26f98faec
lifez <lifez999@gmail.com>
2018-06-28 10:48:57 +0700

    Rename package

--- setup.py
+++ setup.py
@@ -2,18 +2,19 @@ from setuptools import setup
 
 setup(name='django-celery-task-trigger',
       description='Dynamic Task trigger for django-celery-task-broker ',
-      version='1.0.0',
-      url='https://github.com/massenz/filecrypt',
+      version='0.1.0',
+      url='https://github.com/lifez/django-celery-task-trigger',
       author='Prontotools',
       author_email='prontotools@prontomarketing.com',
       license='MIT',
       classifiers=[
-          'FRAMEWORK :: DJANGO',
-          'SYSTEM :: SOFTWARE DISTRIBUTION',
-          'Software Development :: Object Brokering',
-          'LICENSE :: OSI APPROVED :: MIT LICENSE',
-          'Programming Language :: Python :: 3'
+          'Framework :: Django',
+          'Topic :: Software Development :: Object Brokering',
+          'Topic :: System :: Distributed Computing',
+          'Topic :: Utilities',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.6'
       ],
-      packages=['django-celery-task-trigger'],
+      packages=['django_celery_task_trigger'],
       install_requires=['requests>=2.19.1']
 )
